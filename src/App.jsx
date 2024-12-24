ea0c49607287b1f0880bc388889216cb092f6e5b
Mayank Yadav <mayankyadav7249@gmail.com>
2024-12-24 22:38:13 +0530

    my first commit "before develop"

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='container'>
+
+      <div className='calc'></div>
+      <h1 id='input'>0</h1>
+      Hello
+    </div>
+  
+  )
+}
+
+export default App
